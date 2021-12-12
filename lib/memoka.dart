44362520ae3415437e62eefce24e8af5b9d8bbce
byounghaKim <puzi131@gmail.com>
2021-12-12 13:24:45 +0900

    메모카 데이터 연결

--- lib/memoka.dart
+++ lib/memoka.dart
@@ -1,46 +1,62 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-typedef Matrix4 Transformer();
+typedef Transformer = Matrix4 Function();
+
+enum MemokaStatus { init, next, previous }
 
 class Memoka extends StatefulWidget {
   final String front;
   final String back;
   final int page;
-  final bool? isFront;
+  final MemokaStatus status;
+  final VoidCallback nextCallback;
+  final VoidCallback previousCallback;
 
-  const Memoka(
-      {Key? key,
-      required this.front,
-      required this.back,
-      required this.page,
-      this.isFront})
-      : super(key: key);
+  const Memoka({
+    Key? key,
+    required this.front,
+    required this.back,
+    required this.page,
+    required this.status,
+    required this.nextCallback,
+    required this.previousCallback,
+  }) : super(key: key);
 
   @override
   MemokaState createState() => MemokaState();
 }
 
 class MemokaState extends State<Memoka> with TickerProviderStateMixin {
-  final int _revolutionValue = 100;
-  final int _revolutionTime = 100;
   final int _rotationTime = 200;
-  double _horizontalDelta = 0;
-  double _verticalDelat = 0;
+  final int _alignTime = 100;
+  // 드래그 임계치
+  final double threshold = 50;
+
   late Transformer _transformer;
-
-  /// 시작시 원위치로
-  late AnimationController _beginController;
-
-  ///바깥으로 나가는 컨트롤러
-  late AnimationController _exitController;
 
   /// 스스로 회전하는 컨트롤러
   late AnimationController _rotationController;
 
+  /// 위치 컨트롤러
+  late AnimationController _alignController;
+  late Animation<Alignment> _animation;
+  late AnimationController _nextAlignController;
+  late Animation<Alignment> _nextAlignAnimation;
+
+  /// 되돌아오는 컨트롤러
+  late AnimationController _returnController;
+  late Animation<Alignment> _returnAnimation;
+
   late Widget frontWidget;
   late Widget backWidget;
   late Widget contents;
+
+  Alignment _dragAlignment = Alignment.center;
+  final Alignment _rightAlign = const Alignment(10, 0);
+  final Alignment _leftAlign = const Alignment(-10, 0);
+
+  double _dx = 0;
 
   @override
   void initState() {
@@ -48,90 +64,135 @@ class MemokaState extends State<Memoka> with TickerProviderStateMixin {
     frontWidget = Text(widget.front);
     backWidget = Text(widget.back);
     contents = frontWidget;
-    initController();
+    _initController();
+    _initAlignment();
     super.initState();
   }
 
   /// 애니메이션 컨트롤러 초기화
-  void initController() {
-    _beginController = AnimationController(
-        vsync: this, duration: Duration(milliseconds: _revolutionTime))
-      ..addListener(() {
-        setState(() {
-          if (_beginController.isCompleted) {}
-        });
-      });
-    _exitController = AnimationController(
-        vsync: this, duration: Duration(milliseconds: _revolutionTime))
-      ..addListener(() {
-        setState(() {
-          if (_exitController.isCompleted) {
-            // widget.nextMemokaCallback();
-            // dispose();
-          }
-          // if (_exitController.value >= 0.5) {
-          //   widget.nextMemokaCallback();
-          // }
-        });
-      });
+  void _initController() {
     _rotationController = AnimationController(
         vsync: this, duration: Duration(milliseconds: _rotationTime))
       ..addListener(() {
         setState(() {});
       });
+
+    _nextAlignController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: _alignTime));
+    _nextAlignController.addListener(() {
+      setState(() {
+        _dragAlignment = _nextAlignAnimation.value;
+        if (_nextAlignAnimation.isCompleted) {
+          debugPrint('next animation end');
+          widget.nextCallback();
+        }
+      });
+    });
+
+    _alignController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: _alignTime));
+    _alignController.addListener(() {
+      setState(() {
+        _dragAlignment = _animation.value;
+        if (_animation.isCompleted) {
+          widget.previousCallback();
+        }
+      });
+    });
+
+    _returnController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: _alignTime));
+    _returnController.addListener(() {
+      setState(() {
+        _dragAlignment = _returnAnimation.value;
+      });
+    });
+  }
+
+  void _initAlignment() {
+    switch (widget.status) {
+      case MemokaStatus.init:
+        _dragAlignment = Alignment.center;
+        break;
+      case MemokaStatus.next:
+        _dragAlignment = _rightAlign;
+        _runReturnAnimation();
+        break;
+      case MemokaStatus.previous:
+        _dragAlignment = _leftAlign;
+        _runReturnAnimation();
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Transform(
       transform: _transformer(),
       // alignment: FractionalOffset(-1, 1),
       alignment: FractionalOffset.center,
       child: GestureDetector(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.8,
-          height: MediaQuery.of(context).size.width * 0.8 * 0.7,
-          color: Colors.amberAccent,
-          child: Stack(
-            children: [
-              // 중앙 컨텐츠
-              Center(
-                  child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: contents,
-              )),
-              // 오른쪽 하단 페이지
-              Positioned(
-                child: Text(widget.page.toString()),
-                right: 10,
-                bottom: 10,
-              ),
-              // 첫페이지 표시
-              Positioned(
-                child: Visibility(
-                  child: Container(
-                    color: Colors.red,
-                    width: 10,
-                    height: 40,
-                  ),
-                  visible: isTopPage(),
+        child: Align(
+          alignment: _dragAlignment,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.width * 0.8 * 0.7,
+            color: Colors.amberAccent,
+            child: Stack(
+              children: [
+                // 중앙 컨텐츠
+                Center(
+                    child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: contents,
+                )),
+                // 오른쪽 하단 페이지
+                Positioned(
+                  child: Text(widget.page.toString()),
+                  right: 10,
+                  bottom: 10,
                 ),
-                top: -20,
-                right: 10,
-              )
-            ],
+                // 첫페이지 표시
+                Positioned(
+                  child: Visibility(
+                    child: Container(
+                      color: Colors.red,
+                      width: 10,
+                      height: 40,
+                    ),
+                    visible: isTopPage(),
+                  ),
+                  top: -20,
+                  right: 10,
+                )
+              ],
+            ),
           ),
         ),
-        // onHorizontalDragUpdate: (details) {
-        //   _horizontalDelta += details.primaryDelta!;
-        //   // debugPrint(horizontalDelta.toString());
-        // },
-        // onHorizontalDragEnd: (details) {
-        //   if (_horizontalDelta <= -100) {
-        //     _horizontalDelta = 0;
-        //     _exitMemoka();
-        //   }
-        // },
+        onPanDown: (details) {
+          _dx = 0;
+        },
+        onPanUpdate: (details) {
+          setState(() {
+            _dragAlignment += Alignment(
+              details.delta.dx / (size.width / 8),
+              details.delta.dy / (size.height / 2),
+            );
+          });
+          _dx += details.delta.dx;
+        },
+        onPanEnd: (details) {
+          debugPrint('dx : ' + _dx.toString());
+          if (_dx < -threshold) {
+            _runNextAnimation();
+          } else if (_dx > threshold) {
+            _runPreviousAnimation();
+          } else {
+            _runReturnAnimation();
+          }
+        },
         onTap: () {
           // 현재 트랜스포머 뒤짚기로 변경
           _rotationMemoka(widget);
@@ -160,37 +221,6 @@ class MemokaState extends State<Memoka> with TickerProviderStateMixin {
     }
   }
 
-  Matrix4 _rightCenter() {
-    return Matrix4.identity()
-      ..setEntry(0, 3, (1 - _beginController.value) * _revolutionValue);
-  }
-
-  Matrix4 _centerLeft() {
-    // return Matrix4.identity()..rotateZ(-(_controller.value) * pi);
-    return Matrix4.identity()
-      ..setEntry(0, 3, -_exitController.value * _revolutionValue);
-  }
-
-  Matrix4 _leftCenter() {
-    return Matrix4.identity()
-      ..setEntry(0, 3, (-1 + _exitController.value) * _revolutionValue);
-  }
-
-  Matrix4 _cneterRight() {
-    return Matrix4.identity()
-      ..setEntry(0, 3, _exitController.value * _revolutionValue);
-  }
-
-  Matrix4 _initPosition() {
-    return Matrix4.identity()..setEntry(0, 3, 0);
-  }
-
-  void resetPosition() {
-    setState(() {
-      _transformer = _initPosition;
-    });
-  }
-
   void _rotationMemoka(Widget widget) {
     _transformer = _rotation;
     setState(() {
@@ -202,29 +232,44 @@ class MemokaState extends State<Memoka> with TickerProviderStateMixin {
     });
   }
 
-  void _exitMemoka() {
-    // widget.nextMemokaCallback();
-    _transformer = _centerLeft;
-    setState(() {
-      _exitController.reset();
-      _exitController.forward();
-    });
+  void _runReturnAnimation() {
+    _returnAnimation = _returnController.drive(
+      AlignmentTween(
+        begin: _dragAlignment,
+        end: Alignment.center,
+      ),
+    );
+    _returnController.reset();
+    _returnController.forward();
   }
 
-  /// 가운데로 돌아오는 애니메이션
-  void returnFromRight() {
-    debugPrint('beginReturn');
-    _transformer = _rightCenter;
-    _beginController.reset();
-    _beginController.forward();
-    _rotationController.reset();
+  void _runNextAnimation() {
+    _nextAlignAnimation = _nextAlignController.drive(
+      AlignmentTween(
+        begin: _dragAlignment,
+        end: _leftAlign,
+      ),
+    );
+    _nextAlignController.reset();
+    _nextAlignController.forward();
+  }
+
+  void _runPreviousAnimation() {
+    _animation = _alignController.drive(
+      AlignmentTween(
+        begin: _dragAlignment,
+        end: _rightAlign,
+      ),
+    );
+    _alignController.reset();
+    _alignController.forward();
   }
 
   @override
   void dispose() {
     _rotationController.dispose();
-    _exitController.dispose();
-    _beginController.dispose();
+    _returnController.dispose();
+    _alignController.dispose();
     super.dispose();
   }
 
--- lib/memoka_body.dart
+++ lib/memoka_body.dart
@@ -12,14 +12,7 @@ class MemokaBody extends StatefulWidget {
   _MemokaBodyState createState() => _MemokaBodyState();
 }
 
-class _MemokaBodyState extends State<MemokaBody>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Alignment> _animation;
-  Alignment _dragAlignment = Alignment.center;
-
-  // 드래그 임계치
-  final double threshold = 50;
+class _MemokaBodyState extends State<MemokaBody> {
   final GlobalKey _memokaKey = GlobalKey();
 
   late MemokaData _memokaData;
@@ -37,12 +30,6 @@ class _MemokaBodyState extends State<MemokaBody>
     initMemokaBody();
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   Future<Excel> readFile() async {
     // TODO 데이터 널 체크
     return await DataManager().readFile();
@@ -52,15 +39,7 @@ class _MemokaBodyState extends State<MemokaBody>
     // TODO 데이터 널 체크
     Excel memokaExcel = await readFile();
     _memokaData = MemokaData(memokaExcel);
-    _memoka = instanceMemoka();
-
-    _controller = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 100));
-    _controller.addListener(() {
-      setState(() {
-        _dragAlignment = _animation.value;
-      });
-    });
+    _memoka = instanceMemoka(MemokaStatus.init);
 
     /// 인스턴스화한 순간은 key의 state가 null임
     /// 해결하기위해 1frame을 skip하고 빌드를 다시함
@@ -71,87 +50,42 @@ class _MemokaBodyState extends State<MemokaBody>
     // });
   }
 
-  double _dx = 0;
-
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return FutureBuilder(
         future: readFile(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData == false) {
             return const CircularProgressIndicator();
           } else {
-            return Center(
-              child: GestureDetector(
-                  onPanDown: (details) {
-                    _dx = 0;
-                    _controller.stop();
-                  },
-                  onPanUpdate: (details) {
-                    setState(() {
-                      _dragAlignment += Alignment(
-                        details.delta.dx / (size.width / 8),
-                        details.delta.dy / (size.height / 2),
-                      );
-                    });
-                    _dx += details.delta.dx;
-                  },
-                  onPanEnd: (details) {
-                    debugPrint('dx : ' + _dx.toString());
-                    if (_dx < -threshold) {
-                      _runNextAnimation();
-                    } else if (_dx > threshold) {
-                      _runPreviousAnimation();
-                    } else {
-                      _runReturnAnimation();
-                    }
-                  },
-                  child: Align(alignment: _dragAlignment, child: _memoka)),
-            );
+            return Center(child: _memoka);
           }
         });
   }
 
-  Memoka instanceMemoka() {
+  void nextCallback() {
+    _memokaData.nextMemoka();
+    setState(() {
+      _memoka = instanceMemoka(MemokaStatus.next);
+    });
+  }
+
+  void _previousCallback() {
+    _memokaData.previousMemoka();
+    setState(() {
+      _memoka = instanceMemoka(MemokaStatus.previous);
+    });
+  }
+
+  Memoka instanceMemoka(MemokaStatus memokaStatus) {
     return Memoka(
       key: GlobalKey(),
       front: _memokaData.getFrontValue(),
       back: _memokaData.getBackValue(),
       page: _memokaData.getPage(),
+      status: memokaStatus,
+      nextCallback: nextCallback,
+      previousCallback: _previousCallback,
     );
-  }
-
-  void _runReturnAnimation() {
-    _animation = _controller.drive(
-      AlignmentTween(
-        begin: _dragAlignment,
-        end: Alignment.center,
-      ),
-    );
-    _controller.reset();
-    _controller.forward();
-  }
-
-  void _runNextAnimation() {
-    _animation = _controller.drive(
-      AlignmentTween(
-        begin: _dragAlignment,
-        end: Alignment(-10, 0),
-      ),
-    );
-    _controller.reset();
-    _controller.forward();
-  }
-
-  void _runPreviousAnimation() {
-    _animation = _controller.drive(
-      AlignmentTween(
-        begin: _dragAlignment,
-        end: Alignment(10, 0),
-      ),
-    );
-    _controller.reset();
-    _controller.forward();
   }
 }
